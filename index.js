5f45628d0c0ed715cf46f4f9386b8b6a99eac463
Rexhy472 <162793129+Rexhy472@users.noreply.github.com>
2026-05-13 22:23:29 +0700

    Initialize Supabase client in index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
 const {
   Client,
   GatewayIntentBits,
